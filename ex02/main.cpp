347cb9fe3d365216e0a7fcdcda7d0fcf7b3f2095
zanikin <zanikin@student.42yerevan.am>
2024-11-18 12:18:25 +0400

    ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,4 +5,13 @@ int	main() {
 	const std::string	str = "HI THIS IS BRAIN";
 	const std::string	*stringPTR = &str;
 	const std::string	&stringREF = str;
+
+	std::cout << "The memory address of the string variable: " << &str << '\n';
+	std::cout << "The memory address held by stringPTR: " << stringPTR << '\n';
+	std::cout << "The memory address held by stringREF: " << &stringREF << '\n';
+
+	std::cout << "The value of the string variable: " << str << '\n';
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << '\n';
+	std::cout << "The value pointed to by stringREF: " << stringREF << '\n';
+	return 0;
 }
